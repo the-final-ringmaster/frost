716088a81a949fc1717054c18276d2e576841f57
unknown <mjminkus@gmail.com>
2020-07-12 14:39:41 +0100

    Changed the placeholder a little bit...

--- imodule.py
+++ imodule.py
@@ -4,7 +4,7 @@ master=tk.Tk()
 root=tk.Canvas(master, width=500, height=350, bg="black")
 root.pack()
 
-root.create_text(250, 175, fill="gray6", text="This is a test.", font="Arial 60")
-root.create_text(250, 175, fill="white", text="This is a test.", font="Arial 40")
+root.create_text(250, 175, fill="gray6", text="The cake is a lie", font="Arial 60")
+root.create_text(250, 175, fill="white", text="The cake is a lie", font="Arial 40")
 
 tk.mainloop()
