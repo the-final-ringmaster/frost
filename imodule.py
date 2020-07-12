974f11f0dd52dba93266588ff60480cf9e785e56
unknown <mjminkus@gmail.com>
2020-07-12 11:05:12 +0100

    Hmm

--- imodule.py
+++ imodule.py
@@ -1,1 +1,10 @@
-#Tosticols :)
+import tkinter as tk
+
+master=tk.Tk()
+root=tk.Canvas(master, width=500, height=350, bg="black")
+root.pack()
+
+root.create_text(250, 175, fill="gray6", text="This is a test.", font="Arial 60")
+root.create_text(250, 175, fill="white", text="This is a test.", font="Arial 40")
+
+tk.mainloop()
